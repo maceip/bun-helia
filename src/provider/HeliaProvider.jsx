303774ccc5fc9dbb33797a2b14d9cf0254a880be
rpm <ryan.macarthur@gmail.com>
2023-07-21 17:48:41 -0400

    adding libp2p opts

--- src/provider/HeliaProvider.jsx
+++ src/provider/HeliaProvider.jsx
@@ -10,6 +10,15 @@ import {
   useCallback,
   createContext
 } from 'react'
+import { webRTC, webRTCDirect } from '@libp2p/webrtc'
+import { webSockets } from '@libp2p/websockets'
+import { createLibp2p } from 'libp2p'
+import { MemoryBlockstore } from 'blockstore-core'
+import { MemoryDatastore } from 'datastore-core'
+import { noise } from '@chainsafe/libp2p-noise'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { identifyService } from 'libp2p/identify'
+import { bootstrap } from '@libp2p/bootstrap'
 
 export const HeliaContext = createContext({
   helia: null,
@@ -18,11 +27,52 @@ export const HeliaContext = createContext({
   starting: true
 })
 
+async function createNode () {
+  // the blockstore is where we store the blocks that make up files
+  const blockstore = new MemoryBlockstore()
+
+  // application-specific data lives in the datastore
+  const datastore = new MemoryDatastore()
+
+  // libp2p is the networking layer that underpins Helia
+  const libp2p = await createLibp2p({
+    datastore,
+    transports: [
+      webRTC(),webRTCDirect(), webSockets(),
+    ],
+    connectionEncryption: [
+      noise()
+    ],
+    streamMuxers: [
+      yamux()
+    ],
+    peerDiscovery: [
+      bootstrap({
+        list: [
+          '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+          '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+          '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
+        ]
+      })
+    ],
+    services: {
+      identify: identifyService()
+    }
+  })
+
+  return await createHelia({
+    datastore,
+    blockstore,
+    libp2p
+  })
+}
+
 export const HeliaProvider = ({ children }) => {
   const [helia, setHelia] = useState(null)
   const [fs, setFs] = useState(null)
   const [starting, setStarting] = useState(true)
   const [error, setError] = useState(null)
+
 
   const startHelia = useCallback(async () => {
     if (helia) {
@@ -35,7 +85,7 @@ export const HeliaProvider = ({ children }) => {
     } else {
       try {
         console.info('Starting Helia')
-        const helia = await createHelia()
+        const helia = await createNode()
         setHelia(helia)
         setFs(unixfs(helia))
         setStarting(false)
